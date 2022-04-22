23ed5f26a9860976be5a712cdd4be4c1cec88825
Ankush Bahekar <ankushmb9@gmail.com>
2022-04-22 19:26:58 +0530

    [Ankush] Add: Calculated Daily Employee Wage based on part time or full time work

--- Check_Emp.js
+++ Check_Emp.js
@@ -1,5 +1,6 @@
 //UC1 - Check Employee is present or Absent
 
+/*
 const Is_ABSENT=0;
 let empCheck=Math.floor(Math.random()*10)%2;
 
@@ -11,3 +12,28 @@ if(empCheck==Is_ABSENT)
 else{
     console.log("Empl is Present");
 }
+*/
+
+//  UC2 - Calculate Daily Employee Wage based on part time or full time work
+
+
+let empHRs=0;
+
+let empCheck=Math.floor(MAth.random()*10%3);
+
+switch(empCheck)
+{
+    case IS_PART_TIME:
+          empHrs = PART_TIME_HOURS;
+          break;
+        
+    case IS_FULL_TIME:
+    empHrs = FULL_TIME_HOURS;
+    break;
+  default:
+      empHrs=0;
+
+}
+
+let empWage= empHrs*WAGE_PER_HOUR;
+console.log("EmpWage: "+empWage);

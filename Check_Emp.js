786164fc96fa596baf650bb4b291ac7c191096d2
Ankush Bahekar <ankushmb9@gmail.com>
2022-04-22 18:15:26 +0530

    [Ankush] Add: Ability to Check Employee Present or Absent

---
+++ Check_Emp.js
@@ -0,0 +1,13 @@
+//UC1 - Check Employee is present or Absent
+
+const Is_ABSENT=0;
+let empCheck=Math.floor(Math.random()*10)%2;
+
+if(empCheck==Is_ABSENT)
+{
+    console.log("Empl is Absent");
+
+}
+else{
+    console.log("Empl is Present");
+}
